529c8adc4256b009a61fd36ad849a622d555e48f
COOLGATTY <coolgatty@gmail.com>
2015-09-23 23:03:58 -0300

    File Creation
    Created main package and class file for the mod

---
+++ src/main/java/com/palaria/palariamod/PalariaMod.java
@@ -0,0 +1,21 @@
+package com.palaria.palariamod;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+
+@Mod(modid = PalariaMod.MODID, version = PalariaMod.VERSION)
+public class PalariaMod 
+{
+	public static final String MODID = "palariamod";
+	public static final String VERSION = "3.0 beta REBORN";
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+		//shit i don't care about
+	}
+	
+}
